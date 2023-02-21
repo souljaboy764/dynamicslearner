908d7eeca47b3f1727f9cb3a26f2a8ca857b6b08
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2023-02-21 14:27:48 +0100

    added more layers

--- model.py
+++ model.py
@@ -4,9 +4,9 @@ from torch import nn
 class Model(nn.Module):
     def __init__(self, num_inputs, num_outputs, num_hidden=100):
         super(Model, self).__init__()
-        self.__hidden = nn.Linear(num_inputs, num_hidden)
-        self.__output = nn.Linear(num_hidden, num_outputs)
-        self.__activation = nn.LeakyReLU(0.1)
+        self.activation = nn.LeakyReLU(0.1)
+        self.hidden = nn.Sequential(nn.Linear(num_inputs, num_hidden), self.activation, nn.Linear(num_hidden, num_hidden))
+        self.output = nn.Linear(num_hidden, num_outputs)
 
     def forward(self, x):
-        return self.__output(self.__activation(self.__hidden(x)))
+        return self.output(self.activation(self.hidden(x)))
